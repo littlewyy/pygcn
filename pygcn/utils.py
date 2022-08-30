e03a1e546e694c103a6d721b472cd14b205bb6d7
littlewyy <50065094+littlewyy@users.noreply.github.com>
2022-08-30 15:57:43 +0800

    update load_data

--- pygcn/utils.py
+++ pygcn/utils.py
@@ -33,34 +33,43 @@ def load_data(path="../data/cora/", dataset="cora"):
 
     idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
                                         dtype=np.dtype(str))
-    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
-    #调试信息：idx_features_labels[:,1:-1]是什么意思
-    print(features)
-    labels = encode_onehot(idx_features_labels[:, -1])
+    #print(idx_features_labels)
+    #print(idx_features_labels[:,1:-1])
+    # 调试信息：idx_features_labels[:,1:-1]，读取矩阵每一行中，第1维到第-1维的内容，其中最后一列为第-1维。1：-1为左闭右开区间，不包括第-1维
+    # 数据cora.content中，第0维为论文编号，第1维到第-1维之间为论文词向量，第-1维（也就是最后一维）为论文所属领域（标签）
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32) #转换成行压缩稀疏矩阵
+    #print(features)
+    labels = encode_onehot(idx_features_labels[:, -1]) #将论文标签转化为独热码向量
 
     # build graph
-    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
-    idx_map = {j: i for i, j in enumerate(idx)}
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32) #获取论文编号
+    idx_map = {j: i for i, j in enumerate(idx)} #论文编号：序号（从0开始依次），序号相当于为后续论文编号离散化做准备
     edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
-                                    dtype=np.int32)
+                                    dtype=np.int32) #读取引用关系
     edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
-                     dtype=np.int32).reshape(edges_unordered.shape)
-    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-                        shape=(labels.shape[0], labels.shape[0]),
-                        dtype=np.float32)
+                     dtype=np.int32).reshape(edges_unordered.shape) #用新序号重新命名边
+    
+    #print(edges.shape)
+    #print(labels.shape)
 
-    # build symmetric adjacency matrix
+    #adj：邻接矩阵
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]), #shape为点数 * 点数，点数即为论文数目，即为labels总数（含重复）
+                        dtype=np.float32) #将边集转化为coo格式的稀疏矩阵。矩阵中元素数据值为1。
+
+    #将邻接矩阵变换为对称邻接矩阵
     adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
 
-    features = normalize(features)
-    adj = normalize(adj + sp.eye(adj.shape[0]))
+    features = normalize(features) #特征归一化：不理解！！！！！
+    adj = normalize(adj + sp.eye(adj.shape[0])) #邻接矩阵归一化：A' = A + I   A' = D'^-1 A'
 
     idx_train = range(140)
     idx_val = range(200, 500)
     idx_test = range(500, 1500)
 
+    #将稀疏矩阵转换为张量
     features = torch.FloatTensor(np.array(features.todense()))
-    labels = torch.LongTensor(np.where(labels)[1])
+    labels = torch.LongTensor(np.where(labels)[1]) #什么东西？独热码转成常规label?
     adj = sparse_mx_to_torch_sparse_tensor(adj)
 
     idx_train = torch.LongTensor(idx_train)
